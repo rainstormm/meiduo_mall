25aeee4c2fc4aafed93a9b55b60602b45d62ffee
wcache <daniel.w@aliyun.com>
2019-06-17 14:31:36 +0800

    后台管里初始化

---
+++ meiduo_mall/utils/authenticate.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import ModelBackend
+from django.http import HttpRequest
+import re
+from users.models import User
+
+
+class MeiduoModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+
+        try:
+
+            user = User.objects.get(username=username)
+        except:
+            # 如果未查到数据，则返回None，用于后续判断
+            try:
+                user = User.objects.get(mobile=username)
+            except:
+                return None
+
+
+        # 判断密码
+        if user.check_password(password):
+            return user
+        else:
+            return None
+
